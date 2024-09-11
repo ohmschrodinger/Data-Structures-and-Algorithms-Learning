e4ce59badd9b53222e0918281705ceb07b1ee77e
ohmschrodinger <omdhamame@gmail.com>
2024-09-11 15:15:55 +0530

    9 Sept / 2nd Commit

--- 04. Linked List/04_Linked_List_Deletion.c
+++ 04. Linked List/04_Linked_List_Deletion.c
@@ -18,7 +18,6 @@ void traversal(struct Node* ptr)
 struct Node* insert_beg(struct Node* head, int n)
 {
     struct Node* ptr = (struct Node*) malloc(sizeof(struct Node));
-    
     ptr->data = n;
     ptr->next = head;
     
@@ -35,10 +34,8 @@ struct Node* insert_end(struct Node* head, int n)
     {
         p = p->next;
     }
-    
     p-> next = ptr;
     ptr -> next = NULL;
-
     return head;
 }
 
@@ -46,18 +43,63 @@ struct Node* delete_beg(struct Node* head)
 {
     struct Node* ptr = head;
     head = head -> next;
-    
     ptr -> next = NULL;
     free(ptr);
+    return head;
+}
+
+struct Node* delete_index(struct Node* head, int index)
+{
+    struct Node* p = head;
+    struct Node* q = head;
+    int i = 0;
+    while(i < index - 1)
+    {
+        p = p -> next;
+        q = q-> next;
+        i++;
+    }
+    q = q-> next;
+    p-> next = q-> next;
+    q -> next = NULL;
+    free(q);
+    return head;
+}
+
+struct Node* delete_end(struct Node* head)
+{
+    struct Node* ptr = head;
+    while(ptr -> next -> next != NULL)
+    {
+        ptr = ptr -> next;
+    }
+    free(ptr -> next);
+    ptr -> next = NULL;
 
     return head;
+}
+
+// deleting a node with a given value from the linked list
+struct Node* delete_value(struct Node* head, int n)
+{
+    struct Node* ptr = head;
+    struct Node* q = NULL;
+    int index= 0;
+
+    while( ptr -> data != n && ptr != NULL)
+    {
+        q = ptr;
+        ptr = ptr -> next;
+    }
+        q -> next = ptr -> next;
+        free(ptr);
+        return head;
 }
 
 
 int main()
 {
     struct Node* head= NULL;
-    
     int i = 20;
     head = insert_beg(head, 10);
     while(i <= 50){
@@ -68,9 +110,17 @@ int main()
     traversal(head);
     
     head = delete_beg(head);
-
+    
     printf("\n");
-
     traversal(head);
+
+    head = delete_value(head,40);
+    printf("\n");
+    traversal(head);
+
+    head = delete_end(head);
+    printf("\n");
+    traversal(head);
+
     return 0;
 }
