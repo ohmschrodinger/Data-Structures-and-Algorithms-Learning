5fb87555b65a77bb0c5993b56ad7c2d1501eed1c
ohmschrodinger <omdhamame@gmail.com>
2024-09-17 20:48:48 +0530

    17 September 2024/ DLL Question

---
+++ 05. Doubly Linked List/Q03.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+
+struct Node* insert_node(struct Node* head, int n)
+{
+    struct Node* ptr = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* p = head;
+    ptr -> data = n;
+    ptr -> next = NULL;
+    ptr -> prev = NULL;
+
+    if(head == NULL)
+    {
+        return ptr;
+    }
+    else
+    {
+        while(p -> next != NULL )
+        {
+            p = p-> next;
+        }
+        p -> next = ptr;
+        ptr -> prev = p;
+        return head;
+    }
+}
+
+
+void traversal(struct Node* head)
+{
+    struct Node* p = head;
+    while(p != NULL)
+    {
+        printf("%d ", p-> data);
+        p = p-> next;
+    }
+}
+
+
+struct Node* delete_number(struct Node* head, int n)
+{
+    int i = 0;
+    struct Node* p = head;
+    struct Node* q = NULL;
+
+    if(head == NULL)
+    {
+        return head;
+    }
+
+    while(i != n && p != NULL) {
+        q = p;
+        p = p->next;
+        i++;
+    }
+
+    if(p == NULL) {
+        return head;
+    }
+
+  
+    q->next = p->next;
+
+    if(p->next != NULL)
+    {
+        p->next->prev = q;
+    }
+
+    free(p);
+    return head;
+}
+
+int main()
+{
+    struct Node* head = NULL;
+
+    head = insert_node(head, 0);
+    head = insert_node(head, 1);
+    head = insert_node(head, 2);
+    head = insert_node(head, 3);
+    head = insert_node(head, 4);
+    head = insert_node(head, 5);
+
+    traversal(head);
+    printf("\n");
+
+    head = delete_number(head, 3);
+
+    traversal(head);
+
+    return 0;
+}
