0f6c8e08cef80e79f4563c846fe8fc197fd8ac22
ohmschrodinger <omdhamame@gmail.com>
2024-09-26 19:49:12 +0530

    26 Sept 2024/ Tree Searching

--- 08. Tree/02_BinarySearchMethod.c
+++ 08. Tree/02_BinarySearchMethod.c
@@ -55,25 +55,24 @@ struct Node* insert_node(struct Node* root, int n)
     return root;
 }
 
-int flag = 0;
-int inorder_traversal_search(struct Node* root, int n)
+int search_tree(struct Node* root, int n)
 {
-    if(root == NULL)
-    {
-        return 0;
-    }
-
-    inorder_traversal_search(root -> left, n);
-    printf("root data: %d\n", root-> data);
-    int x = root -> data;
-    if(x == n)
+    if(root == NULL) return 0; 
+    if(root -> data == n)
     {
         return 1;
     }
-    inorder_traversal_search(root -> right, n);
-
     
-}
+    if(n < root -> data)
+    {
+        return search_tree(root -> left, n);
+    }
+
+    else
+    {
+        return search_tree(root -> right, n);
+    }
+} 
 
 
 int main()
@@ -86,32 +85,12 @@ int main()
     insert_node(root, 60);
     insert_node(root, 50);
 
-    // inorder_traversal(root);
+    inorder_traversal(root);
 
+    printf("\n");
 
-
-    printf("\n\n\n");
-
-    int result = inorder_traversal_search(root, 100);
+    int result = search_tree(root, 30);
 
     printf("|%d|", result);
     return 0;
 }
-
-
-// int search_tree(struct Node* root, int n)
-// {
-//     int flag = 0;
-//     int key = 1;
-//     while(key != NULL)
-//     {
-//         inorder_traversal(root);
-//         if(key==n)
-//         {
-//             flag++;
-//             return 1;
-//         }
-//     }
-
-//     return 0;
-// }
