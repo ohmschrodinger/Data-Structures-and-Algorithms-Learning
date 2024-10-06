b03d062afe2231a9538f5e46ed0468104c062cf2
ohmschrodinger <omdhamame@gmail.com>
2024-10-06 20:22:02 +0530

    Array Questions

---
+++ 01. Pre-Requisite-CPP/14_TargetSum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Q- find the total number of pairs in the array whose sum is equal to the given value x
+int main()
+{
+    int arr[] = {1,2,3,4,6,5,4,3,7,8,9};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int x = 9;
+    int counter = 0;
+
+    for(int i = 0 ; i < size ; i++)
+    {
+        for(int j = i+1; j< size ;j++)
+        {
+            if(arr[i] + arr[j] == x) counter++;
+        }
+    }
+
+    cout<<counter;
+
+
+    // for number of triplets
+    int triplet_counter = 0;
+
+    for(int i = 0 ; i < size-2 ; i++)
+    {
+        for(int j = i+1; j< size-1 ;j++)
+        {
+            for(int k = j + 1 ; k <size ; k++)
+            {
+
+            if(arr[i] + arr[j] + arr[k] == x) triplet_counter++;
+            }
+        }
+    }
+    cout<<endl;
+    cout<<triplet_counter;
+    return 0;
+}
---
+++ 01. Pre-Requisite-CPP/15_FindUnique.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // find the unique element in array where all the other elements are repeating 
+    int arr[] = {1,2,3,5,2,1,3,3,2,4,4};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    int unique;
+    int counter = 0;
+
+    for(int i = 0 ; i < size ; i++)
+    {
+
+        for(int j = i+1; j< size ;j++)
+        {
+            if(arr[i] == arr[j]) counter++;
+        }
+        if(counter == 0){ unique = arr[i]; break;}
+        counter=0;
+    }
+    cout<<unique;
+
+    return 0;
+}
