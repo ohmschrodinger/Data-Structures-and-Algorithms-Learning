0cc716643897c83d750ad4e72ce80790875b3309
ohmschrodinger <omdhamame@gmail.com>
2024-10-10 22:41:27 +0530

    Search Value

---
+++ 08. Tree/Questions/Q03.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* insert_node(struct Node* root , int n)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if(root == NULL)
+    {
+        newNode -> data = n;
+        newNode -> left = newNode -> right = NULL;
+        return newNode;
+    }
+
+    if(n > root -> data)
+    {
+        root -> right = insert_node(root -> right , n);
+    }
+
+    if(n < root -> data)
+    {
+        root -> left = insert_node(root -> left , n);
+    }
+
+    return root;
+}
+
+
+void inorder_traversal(struct Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    inorder_traversal(root->left);
+    printf("%d ", root->data);
+    inorder_traversal(root->right);
+    
+    return;
+}
+
+int search_node(struct Node* root, int n)
+{
+    if (root == NULL)
+    {
+        return -1;
+
+    }
+    if (root->data == n)
+    {
+        return 1;
+
+    }
+
+    if (n > root->data)
+    {
+        return search_node(root->right, n);
+    }
+
+    else if(n<root->data)
+    {
+        return search_node(root->left, n);   
+    }
+}
+
+
+
+
+int main()
+{
+
+    struct Node* root = NULL;
+    int arr[] = {5,3,1,7,9,2,4};
+    for(int i = 0; i<7;i++)
+    {
+        root = insert_node(root, arr[i]); 
+    }
+
+    inorder_traversal(root);
+    printf("\n");
+    // delete_node(root, 1);
+    // inorder_traversal(root);
+    // printf("\n");
+
+    int result =  search_node(root , 5);
+    printf("%d", result);
+
+    
+    return 0;
+}
