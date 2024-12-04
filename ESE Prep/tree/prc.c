d1761ace5e435fd3744726fc41b3bbd7cf0badf9
ohmschrodinger <omdhamame@gmail.com>
2024-12-04 21:10:24 +0530

    prc

---
+++ ESE Prep/tree/prc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* right;
+    struct Node* left;
+};
+
+struct Node* insertNode(struct Node* root, int n)
+{
+    struct Node* ptr = (struct Node)
+    if(root == NULL)
+    {
+        
+    }
+}
+
+
+
+int main()
+{
+    
+    return 0;
+}
